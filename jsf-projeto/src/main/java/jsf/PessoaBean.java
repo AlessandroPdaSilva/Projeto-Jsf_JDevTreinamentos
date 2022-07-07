e3c2b28c26b8312125b3bb97e7a40d3874c20370
Alessandro <alessandropds55@gmail.com>
2022-07-06 22:30:49 -0300

    Implementação do Hibernate no projeto

---
+++ jsf-projeto/src/main/java/jsf/PessoaBean.java
@@ -0,0 +1,49 @@
+package jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+import dao.DaoGenerico;
+import model.Pessoa;
+
+@ManagedBean(name = "pessoaBean")
+@ViewScoped
+public class PessoaBean {
+
+	Pessoa pessoa = new Pessoa();
+	DaoGenerico<Pessoa> daoPessoa = new DaoGenerico<Pessoa>();
+	
+	public String salvar(){
+		daoPessoa.salvar(pessoa);
+		pessoa = new Pessoa();
+		return "";
+	}
+
+	
+	// GET E SET
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public DaoGenerico<Pessoa> getDaoPessoa() {
+		return daoPessoa;
+	}
+
+	public void setDaoPessoa(DaoGenerico<Pessoa> daoPessoa) {
+		this.daoPessoa = daoPessoa;
+	}
+
+	
+	
+}
---
+++ jsf-projeto/src/test/java/teste/Teste.java
@@ -0,0 +1,25 @@
+package teste;
+
+import javax.persistence.Persistence;
+
+import org.junit.Test;
+
+import dao.DaoGenerico;
+import model.Pessoa;
+
+
+public class Teste {
+	
+	@Test
+	public void testeMain() {
+		DaoGenerico<Pessoa> daoPessoa = new DaoGenerico<Pessoa>();
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome("sdfsdf");
+		pessoa.setSobrenome("sdfsdf");
+		pessoa.setIdade(12);
+		pessoa.setDataNascimento(null);
+		
+		daoPessoa.salvar(pessoa);
+	}
+	
+}
